f5ebd1165efc2f0c74feac823b2247fb1bfcf448
Christoffer Wallenberg <christoffer.wallenberg@zocom.se>
2021-02-09 11:24:44 +0100

    Added files

---
+++ README.md
@@ -0,0 +1,21 @@
+# Lektion 8 februari
+
+## Övningar
+
+Läsa/skriva NodeJS: https://github.com/zocom-christoffer-wallenberg/nodejs-read-write-exercises
+
+## Videos
+
+
+## Artiklar
+
+
+## Inspelningar
+
+Förmiddag: 
+
+Eftermiddag: 
+
+## Tidigare lektioner
+
+https://github.com/Avancerad-JavaScript-med-React-JS-EC/?q=lektion&type=&language=
---
+++ nodejs-example/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+//Första parametern är filens namn
+//Andra parametern är teckenkodningen som är UTF-8
+//Tredje parametern är en funktion som körs efter det är klart. Parametern error populeras om det går fel
+//och content är innehållet i filen
+fs.readFile('hello.txt', 'utf8', (error, content) => {
+    console.log('Längden på innehållet är: ', content.length);
+});
+
+fs.readFile('index.html', 'utf8', (error, content) => {
+    console.log('Längden på innehållet är: ', content.length);
+});
+
+//Text att spara till en textfil
+let quote = 'Be yourself. Everyone else is already taken.';
+
+//Första parametern är filens namn (om den inte finns så skapas den)
+//Andra parametern är det vi vill skriva till textfilen
+//Tredje parametern är en funktion som körs efter det är klart. Parametern error populeras om det går fel
+fs.writeFile('quotes.txt', quote, (error) => {
+    console.log('Quote saved!');
+});
